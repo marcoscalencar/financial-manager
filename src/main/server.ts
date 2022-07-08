f0396bea5323b203d5be0ee5bbacefdbfc499670
Marcos <marcos@getconnect.com.br>
2022-07-08 13:14:36 -0300

    fix(modulealias): fix module_alias

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,23 @@
+require("module-alias/register");
 import setupRoutes from "@/main/config/routes";
+// import * as Sentry from "@sentry/node";
 import "dotenv/config";
 import express from "express";
 
+// Sentry.init({
+//   dsn: "https://16843aed427a4b33b40535ba49dad188@o1308509.ingest.sentry.io/6553876",
+// });
+
 const app = express();
+// app.use(Sentry.Handlers.requestHandler());
+// app.use(Sentry.Handlers.errorHandler());
+// app.use(function onError(err, req, res, next) {
+//   // The error id is attached to `res.sentry` to be returned
+//   // and optionally displayed to the user for support.
+//   res.statusCode = 500;
+//   res.end(res.sentry + "\n");
+// });
+
 app.use(express.json());
 setupRoutes(app);
 
